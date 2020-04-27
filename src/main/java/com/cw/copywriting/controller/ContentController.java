afe3f87e4d8b81fbc054f3c420b111c3338f086e
hukaibo <hfismyangel@163.com>
2020-04-27 22:30:04 +0800

    文案添加

--- src/main/java/com/cw/copywriting/controller/ContentController.java
+++ src/main/java/com/cw/copywriting/controller/ContentController.java
@@ -1,7 +1,11 @@
 package com.cw.copywriting.controller;
 
+import com.cw.copywriting.common.Response;
+import com.cw.copywriting.dto.ContentDto;
 import com.cw.copywriting.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,17 @@ public class ContentController {
 
     @Autowired
     private ContentService contentService;
+
+    @GetMapping(value = "/add")
+    public Response<?> add(@RequestBody ContentDto content) {
+        contentService.addWA(content);
+        return Response.success();
+    }
+
+    @GetMapping(value = "/list")
+    public Response<?> list(@RequestBody ContentDto content) {
+        //todo 
+        contentService.addWA(content);
+        return Response.success();
+    }
 }
--- src/main/java/com/cw/copywriting/service/ContentService.java
+++ src/main/java/com/cw/copywriting/service/ContentService.java
@@ -1,7 +1,16 @@
 package com.cw.copywriting.service;
 
+import com.cw.copywriting.bean.ContentBean;
+import com.cw.copywriting.bean.LabelBean;
+import com.cw.copywriting.bean.LabelContentRelBean;
 import com.cw.copywriting.dao.ContentRepository;
+import com.cw.copywriting.dao.LabelContentRelRepository;
+import com.cw.copywriting.dao.LabelRepository;
+import com.cw.copywriting.dto.ContentDto;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,4 +23,27 @@ public class ContentService {
 
     @Autowired
     private ContentRepository contentRepository;
+    @Autowired
+    private LabelRepository labelRepository;
+    @Autowired
+    private LabelContentRelRepository labelContentRelRepository;
+
+    public void addWA(ContentDto content) {
+        ContentBean contentBean = new ContentBean();
+        contentBean.setContent(content.getContent());
+        contentRepository.findOne(Example.of(contentBean)).ifPresent(e -> {
+            throw new RuntimeException("文案已重复");
+        });
+        ContentBean dataObj = new ContentBean();
+        BeanUtils.copyProperties(content, dataObj);
+        dataObj = contentRepository.save(dataObj);
+
+        LabelBean qo = new LabelBean();
+        qo.setLabelName(content.getTag());
+        LabelBean thisLabel = labelRepository.findOne(Example.of(qo)).orElseGet(() -> labelRepository.save(qo));
+        LabelContentRelBean rel = new LabelContentRelBean();
+        rel.setLabel_id(thisLabel.getId());
+        rel.setContentId(dataObj.getId());
+        labelContentRelRepository.save(rel);
+    }
 }
